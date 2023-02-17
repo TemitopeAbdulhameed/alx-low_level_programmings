690b4620cd4e0faac6b56bd0b9586f8f4bcbbe55
Abdulhameed Aishat Temitope <temitopeaishat4@gmail.com>
2023-02-17 10:42:55 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ *main - print all combinations of three different digits
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int one;
+	int ten;
+	int hundred;
+
+	for (hundred = '0'; hundred <= '9'; hundred++) /*hundreds place*/
+	{
+		for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
+		{
+			for (one = (ten + 1); one <= '9'; one++) /*ones*/
+			{
+				putchar(hundred);
+				putchar(ten);
+				putchar(one);
+				if (hundred != '7' || ten != '8' || one != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+
