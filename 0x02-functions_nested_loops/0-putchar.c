a587c6c88dbecac1880961e8e7240c798e2e0f62
Abdulhameed Aishat Temitope <temitopeaishat4@gmail.com>
2023-02-21 07:30:10 -0800

    main.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main -Entry point
- * Return Always 0
+ * main - print _putchar, followed by new line
+ * Return: 0
  */
 
 int main(void)
 {
-       	_putchar('');
-       	_putchar('p');
+	_putchar('_');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
@@ -16,7 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
-
-
