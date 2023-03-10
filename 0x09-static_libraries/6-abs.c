8e756d1775f56bd88aaf6349a448daab72295afd
Abdulhameed Aishat Temitope <temitopeaishat4@gmail.com>
2023-03-10 12:01:01 -0800

    libmy.a

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,5 +1,18 @@
+#include <stdio.h>
 #include "main.h"
-int _abs(int n)
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @a: the int to check
+ *
+ * Return: the absolute value of int
+ */
+int _abs(int a)
 {
+	if (a >= 0)
+		return (a);
+	return (-a);
 }
+
 
